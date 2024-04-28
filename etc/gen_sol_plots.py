e457ff38eec6948cd42ad01a1a67b6d25becfab1
stevemcgov <svenmcgov@protonmail.com>
2024-04-28 15:25:52 -0400

    Updated gen_sol_plots. Added script for RK45 vs FIE23.
    Cleaned up run_vanderpol.

--- etc/gen_sol_plots.py
+++ etc/gen_sol_plots.py
@@ -11,7 +11,7 @@ from filtered_ie23_py import (
 )
 
 
-def _plot_sol(ranges, ode_sol, names):
+def _plot_sol(ranges, ode_sol, names, show_title=False):
     for r, sol, name in zip(ranges, ode_sol, names):
         t = np.linspace(r[0], r[1], 10001)
         y = sol(t)
@@ -19,7 +19,8 @@ def _plot_sol(ranges, ode_sol, names):
         plt.grid(True)
         plt.ylabel('y axis')
         plt.xlabel('t axis')
-        plt.title(f"y = {name}")
+        if show_title:
+            plt.title(f"y = {name}")
         plt.savefig(f"{name}.jpg")
         plt.show()
         plt.close()
@@ -33,7 +34,8 @@ def _plot_sol_lam(r, ode_sol, set_lambda, lams, name):
         plt.grid(True)
         plt.ylabel('y axis')
         plt.xlabel('t axis')
-        plt.title(f"y = {name}; Lambda = {lam}")
+        if show_title:
+            plt.title(f"y = {name}; Lambda = {lam}")
         plt.savefig(f"{name}_lambda_{lam}.jpg")
         plt.show()
         plt.close()
@@ -44,6 +46,6 @@ if __name__ == "__main__":
     ranges = [[0.0, 20.0], [0.0, 2.0], [0.0, 1.0]]
     set_lambda_sharp_transition_ode1(1.0)
     ode_sol = [test_ode_analog_sol, test_ode_sol, sharp_transition_ode1_sol]
-    names = ["Model ODE Analog", "Model ODE", "Sharp Transition"]
+    names = ["Model ODE Analog", "Model ODE", "Sharp Transition 1"]
     _plot_sol(ranges, ode_sol, names)
     _plot_sol_lam([0.0, 10.0], test_ode_analog_sol, set_lambda_test_ode_analog, [1, 3, 5, 5.7, 6], "Model ODE Analog")
---
+++ filtered_ie23_py/numerical_tests/vanderpol_filtered_ie23_vs_rk45.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.integrate import solve_ivp
+
+from filtered_ie23_py import (
+    # bdf2,
+    filtered_ie23,
+    vanderpol_deriv,
+    set_mu,
+)
+
+t_ranges = [
+    [0.0, 50.0],
+    [0.0, 50.0],
+    [0.0, 100.0],
+    [0.0, 200.0],
+    [0.0, 500.0],
+    [0.0, 1500.0],
+]
+y_init = np.array([1.0, 0.0])
+num_steps = 1000000
+
+dt = 0.001
+tol = 0.0075
+max_steps = 1000000
+mus = [1.0, 2.0, 5.0, 10.0, 100.0, 200.0]
+# num_step = 10000
+
+print(f"Parameter mu & Final t value & Final x value & Method Difference")
+for mu, t_range in zip(mus, t_ranges):
+    set_mu(mu)
+    t = np.linspace(t_range[0], t_range[1], 10000)
+    sol = solve_ivp(vanderpol_deriv, t_range, y_init, t_eval=t)
+    plt.plot(t, sol.y[0], "r-")
+    # t_bdf2, y_bdf2 = bdf2(vanderpol_deriv, t_range, y_init, num_step)
+    # plt.plot(t_bdf2, y_bdf2[:, 0], "r-")
+    t_filtered_ie23, y_filtered_ie23, e = filtered_ie23(
+        vanderpol_deriv, t_range, y_init, dt, tol, max_steps
+    )
+    print(
+        "Filtered-IE23 diff from RK45 at final step: " +
+        f"{abs(y_filtered_ie23[-1, :][0] - sol.y[0][-1]):.5E}"
+    )
+    # print(f"Given t range: {t_range[1]}")
+    # print(f"Final t value returned from Filtered-IE23: {t_filtered_ie23[-1]}")
+    # print(f"Final t value from np.linspace: {t[-1]}")
+    print(f"{mu} & {t_range[1]} & {y_filtered_ie23[-1, :][0]:.5E} &" + \
+        f"{sol.y[0][-1]:.5E} & {abs(y_filtered_ie23[-1, :][0] - sol.y[0][-1]):.5E}")
+    plt.plot(t_filtered_ie23, y_filtered_ie23[:, 0], "b-", linewidth=1.5)
+    plt.grid(True)
+    plt.ylabel('x axis')
+    plt.xlabel('t axis')
+    plt.savefig(f"plots/vanderpol_filtered_ie23_vs_rk45_mu_{mu}.jpg")
+    # plt.show()
+    plt.close()
